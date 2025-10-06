c02e15a1736457f7f5897b3d7eb8ef384ff41f53
Jedsam <fgokgoz02@hotmail.com>
2025-10-06 10:53:49 +0300

    added base structure and basic implementation of the product list

--- frontend/app/src/main/java/com/example/frontendinternship/MainActivity.kt
+++ frontend/app/src/main/java/com/example/frontendinternship/MainActivity.kt
@@ -6,23 +6,26 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.room.Room
 import com.example.frontendinternship.ui.theme.FrontendInternshipTheme
-import java.util.Vector
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +48,7 @@ class MainActivity : ComponentActivity() {
       val plu20ProductList: List<Product>  = productDao.loadAllByVat(vatValue = 20)
     setContent {
       FrontendInternshipTheme {
-            MainAppScreen(plu0ProductList.size)
+            MainAppScreen(plu0ProductList,plu1ProductList,plu10ProductList,   plu20ProductList)
       }
     }
   }
@@ -53,11 +56,27 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun AppPreview (){
-    MainAppScreen(2)
+    val plu0ProductList: List<Product> = listOf(
+        Product(
+            id = 1,
+            productName =  "product1",
+            vatRate = 0,
+            price = "5.0"
+        )
+    )
+    //val plu1ProductList: List<Product> = productDao.loadAllByVat(vatValue = 1)
+    // val plu10ProductList: List<Product>  = productDao.loadAllByVat(vatValue = 10)
+    // val plu20ProductList: List<Product>  = productDao.loadAllByVat(vatValue = 20)
+    MainAppScreen(plu0ProductList, null, null, null)
 }
 
 @Composable
-fun MainAppScreen(Size:Int) {
+fun MainAppScreen(
+    plu0ProductList: List<Product>,
+    plu1ProductList: List<Product>?,
+    plu10ProductList: List<Product>?,
+    plu20ProductList: List<Product>?
+) {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = { TopBar() },
@@ -65,12 +84,25 @@ fun MainAppScreen(Size:Int) {
         Column(
             modifier = Modifier
                 .padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp),
         ) {
-            Text(
-                modifier = Modifier.padding(8.dp),
-                text = Size.toString()
-            )
+            Column(
+                verticalArrangement = Arrangement.spacedBy(24.dp),
+            ) {
+                Row (horizontalArrangement = Arrangement.spacedBy(24.dp)){
+                    for (product0 in plu0ProductList) {
+                        Box(
+                            modifier = Modifier
+                                .size(100.dp)
+                                .clip(RoundedCornerShape(size = 16.dp))
+                                .background(Color.Blue),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = product0.productName + "\n" + product0.price)
+                        }
+                }
+            }
+            }
         }}
 }
 
