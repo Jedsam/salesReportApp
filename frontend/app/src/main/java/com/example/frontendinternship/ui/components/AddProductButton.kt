3d34d7ed2fb84a0cd1c630546e5a042c967198bf
Jedsam <fgokgoz02@hotmail.com>
2025-10-28 15:51:23 +0300

    added addproduct button for real this time...

---
+++ frontend/app/src/main/java/com/example/frontendinternship/ui/components/AddProductButton.kt
@@ -0,0 +1,37 @@
+package com.example.frontendinternship.ui.components
+
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.frontendinternship.ui.theme.FrontendInternshipTheme
+import com.example.frontendinternship.ui.theme.LocalTextFormat
+
+@Composable
+fun AddProductButton(
+    onButtonPressed: () -> Unit
+) {
+    FloatingActionButton(
+        onClick = { onButtonPressed() },
+        containerColor = MaterialTheme.colorScheme.secondary,
+        shape = CircleShape
+    ) {
+        Text(
+            text = "+",
+            fontSize = LocalTextFormat.current.sizeMain,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AddProductButtonPreview() {
+    FrontendInternshipTheme {
+        AddProductButton(
+            onButtonPressed = {}
+        )
+    }
+}
+
