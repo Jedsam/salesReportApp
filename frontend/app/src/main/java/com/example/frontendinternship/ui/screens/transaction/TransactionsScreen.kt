b775a1cc587fd14a66feadcbf7c43d8708fe0fcd
Jedsam <fgokgoz02@hotmail.com>
2025-11-12 11:25:58 +0300

    removed useless button

--- frontend/app/src/main/java/com/example/frontendinternship/ui/screens/transaction/TransactionsScreen.kt
+++ frontend/app/src/main/java/com/example/frontendinternship/ui/screens/transaction/TransactionsScreen.kt
@@ -1,13 +1,7 @@
 package com.example.frontendinternship.ui.screens.transaction
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -15,7 +9,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -24,11 +17,9 @@ import androidx.navigation.compose.rememberNavController
 import com.example.frontendinternship.domain.model.TransactionModel
 import com.example.frontendinternship.domain.usecase.transaction.ILoadTransactionsUseCase
 import com.example.frontendinternship.ui.components.MyScaffold
-import com.example.frontendinternship.ui.components.RoundedButton
 import com.example.frontendinternship.ui.components.TransactionList
 import com.example.frontendinternship.ui.navigation.Screen
 import com.example.frontendinternship.ui.theme.FrontendInternshipTheme
-import com.example.frontendinternship.ui.theme.LocalColors
 import com.example.frontendinternship.ui.theme.LocalDimensions
 import com.example.frontendinternship.ui.theme.LocalPadding
 import com.example.frontendinternship.ui.theme.LocalTextFormat
@@ -75,28 +66,7 @@ fun TransactionsScreen(
         },
         screenText = "Transactions",
         bottomBar = {
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.padding(LocalPadding.current.Small)
-                ) {
-                    RoundedButton(
-                        buttonText = "Go to Basket ( )",
-                        onButtonPress = {
-                            navController.navigate(Screen.Basket.route)
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth(0.7f)
-                            .padding(horizontal = LocalPadding.current.Small)
-                            .height(LocalDimensions.current.viewNormalPlus),
-                        borderColor = LocalColors.current.darkGreen,
-                        containerColor = LocalColors.current.darkGreen,
-                    )
-                }
-            }
+
         },
     ) { innerPadding ->
         TransactionList(
